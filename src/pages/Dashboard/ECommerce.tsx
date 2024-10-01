f803c9e776b8170f325fcab598a92a19362fb158
Tushar Dubey <145278723+TusharDubey07@users.noreply.github.com>
2024-10-01 15:53:08 +0530

    initial commit

---
+++ src/pages/Dashboard/ECommerce.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import CardDataStats from '../../components/CardDataStats';
+import ChartOne from '../../components/Charts/ChartOne';
+import ChartThree from '../../components/Charts/ChartThree';
+import ChartTwo from '../../components/Charts/ChartTwo';
+import ChatCard from '../../components/Chat/ChatCard';
+import MapOne from '../../components/Maps/MapOne';
+import TableOne from '../../components/Tables/TableOne';
+import TableTwo from '../../components/Tables/TableTwo';
+
+const ECommerce: React.FC = () => {
+  return (
+    <>
+      
+        <TableTwo/>
+  
+    </>
+  );
+};
+
+export default ECommerce;
